abca02cbf3e72562be570fa5f23cac6244f6e836
Someonepic <liammitchelcarney.au@gmail.com>
2018-07-01 17:41:29 +1000

    DNS simple record update logic implemented settings saving and loading implemented.

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,56 @@
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+
+import java.awt.event.ActionEvent;
+import java.io.*;
+import java.net.URL;
+
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import javafx.util.Duration;
+import okhttp3.*;
 
 /**
- *   Permission is hereby granted, free of charge, to any person obtaining a copy of this
- *   software and associated documentation files (the "Software"), to deal in the Software
- *   without restriction, including without limitation the rights to use, copy, modify,
- *   merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- *   permit persons to whom the Software is furnished to do so, subject to the following
- *   conditions:
- *
- *   The above copyright notice and this permission notice shall be included in all copies
- *   or substantial portions of the Software.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- *   INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- *   PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- *   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
- *   CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- *   OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 public class Main extends Application {
 
+    public  TextField account_id = new TextField();
+    public  TextField email_Address = new TextField();
+    public  PasswordField password_API = new PasswordField();
+    public  TextField domain = new TextField();
+    public  TextField recordID = new TextField();
+    public  TextField name = new TextField();
+    public  TextField ipAddress = new TextField();
+    public  TextField update_Frequency = new TextField();
+
+
     public void start(Stage primaryStage) throws Exception {
         Stage window = primaryStage;
+
+        loadSettings();
 
 
         window.setTitle("DNS Simple Updater");
@@ -50,10 +70,8 @@ public class Main extends Application {
         CheckBox autostart = new CheckBox();
         Label autostart_Label = new Label("Automatically Start DNS Simple Updater on logon");
 
-        topGridPane.add(autostart_Label , 1 ,0);
-        topGridPane.add(autostart,0,0);
-
-
+        topGridPane.add(autostart_Label, 1, 0);
+        topGridPane.add(autostart, 0, 0);
 
 
         TabPane tabPane = new TabPane();
@@ -64,7 +82,6 @@ public class Main extends Application {
 
         GridPane MainBorderPane = new GridPane();
         tab1.setContent(MainBorderPane);
-
 
 
         GroupBox DNS_Credentials = new GroupBox();
@@ -80,24 +97,24 @@ public class Main extends Application {
         dNS_Credentials_Grid.getColumnConstraints().add(DNS_Credentials_CC);
 
 
+        Label email_Address_Label = new Label("Email Address:  ");
+        Label password_API_Label = new Label("Password / API Token:  ");
+        Label account_id_Label = new Label("Account Id:  ");
 
 
-
-
-        Label email_Address_Label = new Label("Email Address:");
-        Label password_API_Label = new Label("Password / API Token:");
-        TextField email_Address = new TextField();
-        TextField password_API = new TextField();
 
         email_Address.setPromptText("Enter your email address");
         password_API.setPromptText("Enter your API token");
+        account_id.setPromptText("Enter Account Id");
 
-        dNS_Credentials_Grid.add(email_Address_Label, 0,0);
-        dNS_Credentials_Grid.add(email_Address,1,0);
-        dNS_Credentials_Grid.add(password_API_Label,0,1);
-        dNS_Credentials_Grid.add(password_API, 1,1);
+        dNS_Credentials_Grid.add(email_Address_Label, 0, 0);
+        dNS_Credentials_Grid.add(email_Address, 1, 0);
+        dNS_Credentials_Grid.add(password_API_Label, 0, 1);
+        dNS_Credentials_Grid.add(password_API, 1, 1);
+        dNS_Credentials_Grid.add( account_id_Label, 0,2);
+        dNS_Credentials_Grid.add( account_id, 1,2);
 
-        MainBorderPane.add(DNS_Credentials,0,1);
+        MainBorderPane.add(DNS_Credentials, 0, 1);
 
         DNS_Record.setText("DNS Record");
 
@@ -113,10 +130,12 @@ public class Main extends Application {
         Label name_Label = new Label("Name:  ");
         Label ipAddress_Label = new Label("IP Adress:  ");
 
-        TextField domain = new TextField();
-        TextField recordID = new TextField();
-        TextField name = new TextField();
-        TextField ipAddress = new TextField();
+
+
+        domain.setPromptText("Enter Domain Name");
+        recordID.setPromptText("Enter Record Id");
+        name.setPromptText("Enter Name");
+
 
         dNS_Record_Grid.add(domain_Label, 0, 0);
         dNS_Record_Grid.add(recordID_Label, 0, 1);
@@ -128,25 +147,24 @@ public class Main extends Application {
         dNS_Record_Grid.add(name, 1, 2);
         dNS_Record_Grid.add(ipAddress, 1, 3);
 
-        MainBorderPane.add(DNS_Record,0,2);
+        MainBorderPane.add(DNS_Record, 0, 2);
 
 
         GridPane dns_schedule_grid = new GridPane();
         Schedule.setText("Schedule");
         Label update_Frequency_Label = new Label("Update Frequencey:  ");
-        TextField update_Frequency = new TextField();
+
         update_Frequency.setPromptText("Frequency in Min");
 
         dns_schedule_grid.getColumnConstraints().add(DNS_Credentials_CC);
 
-        dns_schedule_grid.add(update_Frequency_Label, 0,0);
-        dns_schedule_grid.add(update_Frequency,1,0);
+        dns_schedule_grid.add(update_Frequency_Label, 0, 0);
+        dns_schedule_grid.add(update_Frequency, 1, 0);
 
         Schedule.setContent(dns_schedule_grid);
-        MainBorderPane.add(Schedule, 0,3);
+        MainBorderPane.add(Schedule, 0, 3);
 
-        MainBorderPane.add(TopGroup, 0 ,0);
-
+        MainBorderPane.add(TopGroup, 0, 0);
 
 
         Button Ok = new Button("Ok");
@@ -155,24 +173,45 @@ public class Main extends Application {
         Cancel.setMinWidth(60);
 
         HBox toolbarhbox = new HBox();
-        toolbarhbox.getChildren().addAll(Ok,Cancel);
+        toolbarhbox.getChildren().addAll(Ok, Cancel);
         Region region2 = new Region();
         HBox.setHgrow(region2, Priority.ALWAYS);
 
+        ipAddress.setText(getIp());
+        ipAddress.setEditable(false);
 
         ToolBar toolBar = new ToolBar(
 
-               region2,toolbarhbox
+                region2, toolbarhbox
 
 
         );
+        Ok.setOnAction(e -> {
+
+            saveSettings();
+            System.out.println("Trying to update");
+            try {
+                UpdateDnsRecord(recordID.getText(), domain.getText(), account_id.getText(), password_API.getText(), ipAddress.getText());
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
 
 
+        });
 
-        MainBorderPane.add(toolBar, 0 ,4);
 
-        Scene MainScene = new Scene(tabPane, 320, 430);
+        Cancel.setOnAction(e -> {
 
+            saveSettings();
+            window.close();
+
+
+        });
+
+
+        MainBorderPane.add(toolBar, 0, 4);
+
+        Scene MainScene = new Scene(tabPane, 320, 460);
 
 
         window.initStyle(StageStyle.DECORATED);
@@ -183,5 +222,84 @@ public class Main extends Application {
 
 
     }
+    public static String getIp() throws Exception {
+        URL whatismyip = new URL("http://checkip.amazonaws.com");
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+            String ip = in.readLine();
+            return ip;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void UpdateDnsRecord( String recordId, String domain, String accountId, String password, String ip ) throws IOException {
+
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, "{\"content\": \""+ ip + "\"}");
+
+        Request request = new Request.Builder()
+                .url("https://api.dnsimple.com/v2/"+accountId+"/zones/"+ domain +"/records/"+recordId)
+                .patch(body)
+                .addHeader("Authorization", "Bearer "+ password)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Cache-Control", "no-cache")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println(response);
+
+    }
+    public void saveSettings(){
+
+        new File(System.getProperty("user.dir") + "/Settings/").mkdirs();
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+
+                /*
+                   Output the settings file to the directory
+                */
+                new FileOutputStream(System.getProperty("user.dir") + "/Settings/Settings.ini"), "utf-8"))) {
+
+            String s = email_Address.getText() + "\n" + password_API.getText() + "\n" + account_id.getText() + "\n" + domain.getText() + "\n" +recordID.getText() + "\n" + name.getText() + "\n" + update_Frequency.getText();
+
+            writer.write(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void loadSettings() throws IOException {
+
+        if( new File(System.getProperty("user.dir") + "/Settings/Settings.ini").exists()){
+
+            BufferedReader brTest = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/Settings/Settings.ini"));
+            email_Address.setText( brTest .readLine());
+            password_API.setText( brTest .readLine());
+            account_id.setText( brTest .readLine());
+            domain.setText( brTest .readLine());
+            recordID.setText( brTest .readLine());
+            name.setText( brTest .readLine());
+            update_Frequency.setText( brTest .readLine());
+
+
+        };
+
+
+    }
+
+
+
 
 }
